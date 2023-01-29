5c293d567ec703b0d2255e2273633b6eedb33910
Vipul <007vipulrank1@gmail.com>
2023-01-29 06:56:50 +0530

    ADD: cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
+import cors from 'cors'
 import connect from './configs/connectDb.js'
 import UserRouter from './routes/userRoutes.js'
 import errorHandler from './middlewares/errorMiddleware.js'
@@ -19,6 +20,7 @@ const DB_URI = process.env.MONGO_DB_URI.replace('%USER%', process.env.MONGO_DB_U
 await connect(DB_URI)
 
 const app = express()
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 
